b4f76872a4082b91b5756a70badaa9fdcccef8e3
ryan <rcorales@traxiontech.net>
2020-03-20 14:34:23 +0800

    5 commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
-class apache {
-        package { 'httpd':
-                ensure => present,
-        }
+class apache (
+        $package_name  = $apache::params::package_name,
+        $service_name  = $apache::params::service_name,
+        $vhost_dir     = $apache::params::vhost_dir,
+        $conf_dir      = $apache::params::conf_dir,
+        $document_root = $apache::params::document_root,
+        $server_name   = $apache::params::servername,
+        $log_dir       = $apache::params::log_dir
+) inherits apache::params {
+
 
 }
