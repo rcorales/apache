686e9f08753ac24de9c2cc3a0e950dc2a7d5990a
ryan <rcorales@traxiontech.net>
2020-03-23 07:34:12 +0800

    16 commits

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,7 @@ class apache::service ($service_name = $apache::params::service_name)
 {
         service { 'apache':
                 name    => $service_name,
-                enable  => true,
+                enable  => false,
                 ensure  => stooped,
                 require => Package['apache'],
 
