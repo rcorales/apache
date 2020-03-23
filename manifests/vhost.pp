2a423c072f3723cc3546e7640001afcf98555694
ryan <rcorales@traxiontech.net>
2020-03-23 08:49:03 +0800

    18 commits

---
+++ manifests/vhost.pp
@@ -0,0 +1,16 @@
+define apache::vhost:: ($port, $document_root, $servername, $vhost_name = '*', $vhost_dir) {
+
+file { 'index':
+        path    => "${document_root}/index.html",
+        ensure  => file,
+        content => template('apache/index.html.erb'),
+        before  => File['config_file'],
+}
+
+file 'config_file':
+      path    =>  "${vhost_dir/${servername}.conf",
+      content =>  template('apache/vhost.conf.erb'),
+      require => Package['apache'],
+      notify  => Service['apache'],
+
+}      
