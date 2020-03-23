0e75f50548785334bab19b219935c046a52b073c
ryan <rcorales@traxiontech.net>
2020-03-23 10:55:24 +0800

    31 commits

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,12 +21,12 @@ file { $log_dir:
         recurse => true,
 }
 
-#apache::vhost { "default":
-#        port          =>  80,
-#        document_root =>  $document_root,
-#        server_name   =>  $servername,
-#        vhost_dir     =>  $vhost_dir,
+apache::vhost { "default":
+        port          =>  80,
+        document_root =>  $document_root,
+        server_name   =>  $servername,
+        vhost_dir     =>  $vhost_dir,
 
-#}
+}
 
 }
